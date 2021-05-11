3a43d0d8a20974b7ab90bdef00697af84ddc67c6
Evan Grossman <grossmae@gmail.com>
2021-05-11 10:29:56 -0500

    Initial Commit

---
+++ Shared/NestedViewModelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NestedViewModelsApp.swift
+//  Shared
+//
+//  Created by Evan Grossman on 5/11/21.
+//
+
+import SwiftUI
+
+@main
+struct NestedViewModelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
